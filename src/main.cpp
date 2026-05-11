a0a8830a694506fd951519404bbff92c6c908c09
Ivan <zhdanovich2818@gmail.com>
2026-05-11 22:06:08 +0300

    final refactoring commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <vector>
+#include "../include/shelter/utils/DataLoader.h"
 
 int main() {
+    std::vector<Pet*> pets;
+
+    if (DataLoader::loadFromFile("../data/pets.txt", pets)) {
+        for (Pet* pet : pets) {
+            pet->printInfo();
+            delete pet;
+        }
+    } else {
+        std::cout << "File not found!" << std::endl;
+    }
+
     return 0;
 }
+// final check for github display
